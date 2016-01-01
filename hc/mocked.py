854b39042b2ecfa6c331aa3dd4a3dd38c6f23d61
Richard Marko <rmarko@fedoraproject.org>
2016-01-02 00:17:38 +0100

    update mocked printer

--- hc/mocked.py
+++ hc/mocked.py
@@ -1,24 +1,42 @@
+import math
 import random
+
+import collections
 
 from twisted.internet import task
 from twisted.protocols.basic import LineReceiver
 
-from . import log
-from . import gcode
+from . import log, gcode, parse, vars
 
 #from twisted.logger import Logger
 # FIXME: update to ^^ when possible
 
 
-class MockedPrinter(LineReceiver):
+class MockedPrinter(LineReceiver, object):
     delimiter = '\n'
     serial_rx_verbose = True
     serial_tx_verbose = True
     current_line = 0
     failcounter = 0
     warncounter = 0
-    reserved = ['G', 'M', 'T', 'N']
+
     resend_next = False
+
+    temps = collections.OrderedDict()
+    temp_designators = vars.temp_designators
+
+    axes = collections.OrderedDict()
+    axes_designators = vars.axes_designators
+
+    def __init__(self):
+        super(MockedPrinter, self).__init__()
+
+        for d in self.temp_designators:
+            # current, target, power
+            self.temps[d] = [0.0, 0.0, 0]
+
+        for d in self.axes_designators:
+            self.axes[d] = 0.0
 
     def connectionMade(self):
         self.cmd('start')
@@ -79,8 +97,7 @@ class MockedPrinter(LineReceiver):
             self.warn('Empty command received')
             return
 
-        #log.msg('!!! {}'.format(sl[0]))
-        if sl[0] in self.reserved:
+        if sl[0] in vars.gcode_reserved:
             if self.resend_next:
                 self.resend_next = False
                 self.current_line += 1
@@ -113,12 +130,47 @@ class MockedPrinter(LineReceiver):
         self.handle_command(line)
 
     def handle_command(self, line):
+        if 'G0' in line or 'G1' in line:
+            for axis, val in parse.axes(line):
+                self.axes[axis] = val
+
+        if 'G30' in line:
+            #z, c = self.zprobe()
+            z, c = -1.14, 43000
+
+            #z += self.axes['X'] / 10 + self.axes['Y'] / 10
+            z += math.sin(self.axes['X']) + math.cos(self.axes['Y'])
+
+            self.cmd('Z:{:.4f} C:{}'.format(z, c))
+
         if 'M110' in line:
             self.info('Resetting line number')
             self.current_line = 0
 
+        if 'M105' in line:
+            # ok T:25.0 /0.0 @0 T:24.4 /0.0 @0
+            temp_template = '{}: {:.2f} /{:.2f} @{} '
+            out = ''
+            for designator, t in self.temps.items():
+                current, target, power = t
+                out += temp_template.format(designator, current, target, power)
+            self.cmd(out[:-1])
+
         if 'M114' in line:
-            self.cmd('T:23.4 / 220.0 B:38.5 / 60.0')
+            # ok C: X:0.000 Y:0.000 Z:0.000 A:192.031 B:192.031 C:192.031  E:0.000
+            pos_template = '{}:{:.3f} '
+            out = ''
+            for designator, pos in self.axes.items():
+                out += pos_template.format(designator, pos)
+            self.cmd(out[:-1])
+
+        if 'M119' in line:
+            # min_x:0 min_y:0  Probe: 0
+            self.cmd('min_x:0 min_y:0  Probe: 0')
+
+        if 'MULTILINE' in line:
+            for i in range(10):
+                self.cmd('Line {}'.format(i))
 
         self.ack()
 
@@ -126,9 +178,11 @@ class MockedPrinter(LineReceiver):
 class BufferedMockedPrinter(MockedPrinter):
     max_lines = 10
     max_bytes = 256
-    interval = 1
+    interval = 0.5
 
     def __init__(self):
+        super(BufferedMockedPrinter, self).__init__()
+
         self.lines = []
         self.c_lines = 0
         self.c_bytes = 0
