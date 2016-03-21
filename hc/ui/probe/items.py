5d7c94262cb8b1b8af6b016b4dfd708996e77ba3
Richard Marko <rmarko@fedoraproject.org>
2016-03-21 21:53:32 +0100

    drop old code from GCode item

--- hc/ui/probe/items.py
+++ hc/ui/probe/items.py
@@ -145,34 +145,6 @@ class GCode(GLGraphicsItem):
             glCallList(self.list)
             return
 
-        #glClearColor(0, 0, 0, 0)
-        #glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-        #glMatrixMode(GL_MODELVIEW)
-        #glLoadIdentity()
-        #L = 0
-        #glRotate(180, 0, 0, 1)
-        #glRotate(180, 0, 1, 0)
-        ##glDisable(GL_DEPTH_TEST)
-        #glColor3f(0.8, 0.8, 0.8)
-        #self.renderText(20, 20, '{} | {} cmds'.format(
-        #    self.filename, self.gcodesize))
-        #self.renderText(20, 40, '{:.4f} x {:.4f} x {:.4f} mm'.format(
-        #    self.objsize[0], self.objsize[1], self.objsize[2]))
-        #self.renderText(20, 60, '{:.4f} , {:.4f} deg'.format(
-        #    self.rotx, self.roty))
-        #self.renderText(20, 80, 'v: {:.4f} , {:.4f} , {:.4f}'.format(
-        #    ViewerWidget.tranx, ViewerWidget.trany, ViewerWidget.tranz))
-        #self.renderText(20, 100, '{:.4f} x {:.4f} x {:.4f} mm'.format(
-        #    self._oc[0], self._oc[1], self._oc[2]))
-
-        #glPushMatrix()
-        #glTranslate(ViewerWidget.tranx, ViewerWidget.trany, ViewerWidget.tranz)
-        #glScale(ViewerWidget.scale, ViewerWidget.scale, ViewerWidget.scale)
-        #glRotate(float(ViewerWidget.roty), 1.0, 0.0, 0.0)
-        #glRotate(float(ViewerWidget.rotx), 0.0, 0.0, 1.0)
-        #glTranslate(-self._oc[0], -self._oc[1], -self._oc[2])
-
         self.list = glGenLists(1)
         glNewList(self.list, GL_COMPILE) #_AND_EXECUTE)
         glBegin(GL_LINE_STRIP)
@@ -185,9 +157,6 @@ class GCode(GLGraphicsItem):
 
         glCallList(self.list)
 
-        #glPopMatrix()
-        #self.swapBuffers()
-
     def loadGCode(self, filename):
         if self.list:
             glDeleteLists(self.list, 1)
@@ -195,14 +164,8 @@ class GCode(GLGraphicsItem):
 
         self.gcode_points = util.load_gcode_commands(filename)
         s = self.object_size(self.gcode_points)
-#      ViewerWidget.tranx = -s[0]/2
-#      ViewerWidget.trany = -s[2]/2
-        #ViewerWidget.tranz = -s[2] * 3
-#        ViewerWidget.tranz = s[2] * 10
-        #ViewerWidget.scale = -s[1]*3
+
         self.filename = filename
         self.gcodesize = len(self.gcode_points)
         self.objsize = s
         self._recalc_object_center(self.gcode_points)
-
-        #self.update()
