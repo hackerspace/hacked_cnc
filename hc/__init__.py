ada4d6a6adbbb538f8ed9ce3754beca05d001ef8
Richard Marko <rmarko@fedoraproject.org>
2016-04-05 17:01:08 +0200

    fix monitors

--- hc/__init__.py
+++ hc/__init__.py
@@ -1,8 +1,10 @@
 from . import log
 from . import client as hc_client
+from . import monitor as hc_monitor
 from . import server as hc_server
 
 client = hc_client
+monitor = hc_monitor
 server = hc_server
 
 
